539eab673374a290a9821a0b94c16395f3d4490b
Pavel Matcula <dev.plvlml@gmail.com>
2016-09-22 21:27:49 +0200

    Added Golden Section Search

---
+++ golden_section_search.h
@@ -0,0 +1,19 @@
+template <class T, class Function, class Compare = std::less<T>>
+T GoldenSectionSearch(T x1, T x2, Function function, Compare compare = Compare()) {
+  T xL = x1 + (x2 - x1) * (2 - phi);
+  T xR = x2 - (x2 - x1) * (2 - phi);
+  T fL = function(xL);
+  T fR = function(xR);
+  while (x2 - x1 >= eps) {
+    if (compare(fL, fR)) {
+      x2 = xR;
+      xR = xL, xL = x1 + (x2 - x1) * (2 - phi);
+      fR = fL, fL = function(xL);
+    } else {
+      x1 = xL;
+      xL = xR, xR = x2 - (x2 - x1) * (2 - phi);
+      fL = fR, fR = function(xR);
+    }
+  }
+  return (x1 + x2) / 2;
+}

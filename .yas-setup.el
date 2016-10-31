e0c8762e58b8dfd433503508ba5d232ee15fcb14
Pavel Matcula <dev.plvlml@gmail.com>
2016-10-31 20:31:17 +0200

    Refactored and improved insert-header-file-contents Elisp function.

--- .yas-setup.el
+++ .yas-setup.el
@@ -2,5 +2,12 @@
   "Insert contents from a HEADER_FILE located in yasnippet snippets directory."
   (with-temp-buffer
     (insert-file-contents
-      (expand-file-name header-file (car yas-snippet-dirs)))
+      (expand-file-name header-file
+                        (expand-file-name "c++-mode/online_judge/"
+                                          (car yas-snippet-dirs))))
+    (goto-char (point-min))
+    (flush-lines "^ *#ifndef *.*_H_ *$")
+    (flush-lines "^ *#define *.*_H_ *$")
+    (flush-lines "^ *#include *\".*\" *$")
+    (flush-lines "^ *#endif *.*_H_ *$")
     (buffer-string)))
